a0b65533f613248fc93e1da43a334256bd6e90b0
ichebadu <101195914+ichebadu@users.noreply.github.com>
2022-10-18 21:26:42 +0100

    Update write_funcs.c

--- write_funcs.c
+++ write_funcs.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - writes the character c to stdout.
+ * @c: The character to print.
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  * Description: _putchar uses a local buffer of 1024 to call write
