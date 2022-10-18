d2cf908d12ffaaa44695e68cdc0e2ccdb3a498a4
ichebadu <101195914+ichebadu@users.noreply.github.com>
2022-10-18 15:34:35 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _printf - this function produces output according to a format
+ * _printf - this function produces output according to a specified format
  * @format: format string containing the characters and the specifiers
  * Description: this function will call the get_print() function that will
  * determine which printing function to call depending on the conversion
